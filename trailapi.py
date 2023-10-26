c8dcc9c86d9e85d0140e39af9fb4fa7792d07d65
phani123-oss <142466417+phani123-oss@users.noreply.github.com>
2023-10-26 02:37:06 -0700

    Add files via upload

---
+++ trailapi.py
@@ -0,0 +1,15 @@
+from flask import Flask, request,jsonify,render_template
+app = Flask(__name__)
+@app.route("/")
+def show_form():
+    return render_template("index.html")
+
+@app.route("/check_password",methods=['post'])
+def check_password():
+    name = request.form.get("username")
+    password = request.form.get("password")
+    print({name},{password}
+    return "username and password recieved"
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0",port = 8000)
